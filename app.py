6c96ac9c86d540fec06f4277520254b66cd0db48
= <kwangonya@gmail.com>
2019-10-10 22:23:44 +0300

    add DB_URI and create database relations

--- app.py
+++ app.py
@@ -7,13 +7,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 db = SQLAlchemy(app)
 
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
+
+
+db.create_all()
 
 
 @app.route("/")
