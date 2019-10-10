1e218cca34a557efd2d2329dda12563dcb0ebe3d
= <kwangonya@gmail.com>
2019-10-10 12:47:03 +0300

    add SQLAlchemy

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+db = SQLAlchemy(app)
 
 
 @app.route("/")
